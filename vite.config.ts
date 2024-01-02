237f433997e15e5f2b7d1133e45a9e0c59e72503
liuzisheng <1171764830@qq.com>
2024-01-02 16:31:42 +0800

    ci: 配置别名

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,15 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
+import path from "path"
+function _resolve(dir: string) {
+  return path.resolve(__dirname, dir)
+}
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": _resolve("src")
+    }
+  },
   plugins: [react()]
 })
