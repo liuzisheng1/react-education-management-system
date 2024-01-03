4ca916c0dbab13485f3de237b29dc6ff53fb15b7
Zze <3094756826@qq.com>
2024-01-03 18:51:41 +0800

    feat: zustand的demo

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,6 +2,7 @@ import { lazy } from "react"
 import { Navigate, RouteObject } from "react-router-dom"
 const Login = lazy(() => import("@/views/login"))
 const Home = lazy(() => import("@/views/home"))
+const Demo = lazy(() => import("@/views/demo"))
 
 const routes: RouteObject[] = [
   {
@@ -12,6 +13,10 @@ const routes: RouteObject[] = [
     path: "/home",
     element: <Home />,
     children: [{}]
+  },
+  {
+    path: "/demo",
+    element: <Demo />
   },
   {
     path: "/login",
