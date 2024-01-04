0efc7867f4591bede1b1da1b33ebf368125c7969
liuzisheng1 <lzs13333464863@163.com>
2024-01-04 17:39:19 +0800

    fix: dome界面报错

--- src/views/demo/index.tsx
+++ src/views/demo/index.tsx
@@ -1,27 +1,15 @@
-import React, { useState, useEffect } from "react"
-import { Button } from "@/plugins/antd.ts"
-import { getUserInfo } from "@/api"
+import React, { useEffect } from "react"
 import { useCountStore } from "@/store/createStore"
 const DemoIndex: React.FC = () => {
   const { count, increment } = useCountStore()
-  const [state, setState] = useState([])
   console.log(count)
   useEffect(() => {
     console.log(import.meta.env.DEV)
   }, [])
-  const handleGetUserInfo = async () => {
-    const res = await getUserInfo()
-    res && setState(res)
-    console.log(res, state)
-  }
-
   return (
     <div>
       <h1>{count}</h1>
       <button onClick={increment}>加</button>
-      <Button type="primary" onClick={handleGetUserInfo}>
-        按钮
-      </Button>
     </div>
   )
 }
