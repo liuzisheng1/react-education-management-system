48ecc873aa752fb3cd36b82b94c3021b0513dcb0
liuzisheng1 <lzs13333464863@163.com>
2024-01-04 18:23:26 +0800

    feat: storage, proxy跨域

--- src/api/modules/user.ts
+++ src/api/modules/user.ts
@@ -1,4 +1,4 @@
-import { http } from "@/utils/http"
+import { http } from "@/utils/http/index.ts"
 import { BasicResponseModel } from "@/types"
 
 export function getUserInfo() {
--- vite.config.ts
+++ vite.config.ts
@@ -13,8 +13,6 @@ export default defineConfig({
   },
   plugins: [react()],
   server: {
-    host: "127.0.0.1",
-    port: 3000,
     proxy: {
       "/api": {
         target: "http://127.0.0.1:3000",
