fceb45492374e4ad1490f3e83337bcb28c759fa5
liuzisheng1 <lzs13333464863@163.com>
2024-01-06 02:34:36 +0800

    perf: 优化路由守卫

--- src/router/routerGuard.ts
+++ src/router/routerGuard.ts
@@ -1,52 +1,61 @@
 import { useState, useEffect } from "react"
 import { useLocation, useRoutes, Location, useNavigate, NavigateFunction } from "react-router-dom"
+
+// import { useLocation, useNavigate, Location, NavigateFunction } from "react-router-dom"
 import useStorage from "@/utils/storage.ts"
 import { RouteObject } from "@/types"
 
-export const searchRouteDetail = (path: string, routes: RouteObject[]): RouteObject | null => {
+// 封装权限验证逻辑
+const useAuthCheck = (tokenKey: string) => {
+  const storage = useStorage("localStorage")
+  const isTokenValid = storage.getItem(tokenKey)
+  const [authChecked, setAuthChecked] = useState(false)
+  const navigate = useNavigate()
+  useEffect(() => {
+    !authChecked && (!isTokenValid ? navigate("/login") : setAuthChecked(true))
+  }, [navigate, authChecked, isTokenValid])
+
+  return authChecked
+}
+
+// 支持尾递归优化的 searchRouteDetail 函数
+export const searchRouteDetail = (
+  path: string,
+  routes: RouteObject[],
+  acc?: RouteObject | null
+): RouteObject | null => {
   for (const item of routes) {
     if (item.path === path) {
       return item
     }
     if (item.children) {
-      const routeDetail = searchRouteDetail(path, item.children)
+      const routeDetail = searchRouteDetail(path, item.children, acc)
       if (routeDetail) {
         return routeDetail
       }
     }
   }
-  // 如果没有找到匹配的路由，则返回null
-  return null
+  // 如果没有找到匹配的路由，并且acc不存在，则返回null；否则返回acc
+  return acc ?? null
 }
 
-export const guard = (
-  location: Location,
-  navigate: NavigateFunction,
-  routes: RouteObject[]
-): boolean => {
+export const guard = (location: Location, navigate: NavigateFunction, routes: RouteObject[]) => {
   const { pathname } = location
   const routeDetail = searchRouteDetail(pathname, routes)
-  !routeDetail && navigate("/404")
-
-  return true
+  if (!routeDetail) {
+    navigate("/404")
+  }
 }
 
 export const RouterGuard = (routes: RouteObject[]) => {
   const location = useLocation()
   const navigate = useNavigate()
-  const storage = useStorage("sessionStorage")
-  const isTokenValid = storage.getItem("token")
-  const [authChecked, setAuthChecked] = useState(false)
+  const authChecked = useAuthCheck("access_token")
 
   useEffect(() => {
-    if (!authChecked) {
-      !isTokenValid ? navigate("/login") : setAuthChecked(true)
-    }
-  }, [navigate, authChecked])
-
-  useEffect(() => {
-    guard(location, navigate, routes)
+    authChecked && guard(location, navigate, routes)
+    document.documentElement.scrollTo(0, 0)
   }, [location, authChecked, navigate, routes])
-  document.documentElement.scrollTo(0, 0)
+
   return useRoutes(routes as any)
 }
